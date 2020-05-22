5a75f5e3d03b353b91cd5e14a28ba403b775fff7
Khalimov Rustam <okrug102m@gmail.com>
2020-05-22 14:23:55 +0300

    Update example

---
+++ src/Example/Models/TestData.cs
@@ -0,0 +1,13 @@
+﻿using Shuriken;
+
+namespace Example.Models
+{
+    public class TestData:ObservableObject
+    {
+        [Observable]
+        public double ProgressValue { get; set; }
+
+        [Observable]
+        public double ProgressValueSecond { get; set; }
+    }
+}
--- src/Example/ViewModels/TestViewModel.cs
+++ src/Example/ViewModels/TestViewModel.cs
@@ -1,12 +1,33 @@
-﻿using Example.Views;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using Example.Models;
+using Example.Views;
 using FragmentManager.Attributes;
 using FragmentManager.Shuriken;
+using Shuriken;
 
 namespace Example.ViewModels
 {
     [View(typeof(TestView))]
     public class TestViewModel:ObservableFragment
     {
+        [Observable]
+        public TestData Data { get; private set; }
+
+        public TestViewModel()
+        {
+            Data = new TestData();
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Data.ProgressValue++;
+                    Data.ProgressValueSecond += 1.3;
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
 
     }
 }
